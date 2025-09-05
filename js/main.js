f9db89e1c9cedb44cfc97e8c0b24391201c252c2
leoSwing1 <kevinleoswing@gmail.com>
2025-09-05 08:59:39 +0530

    testimonial-bug-cleared

--- js/main.js
+++ js/main.js
@@ -47,11 +47,19 @@ $(document).ready(function(){
     });
 
     $(".testimonials-carousel").owlCarousel({
-        autoplay: true,
-        dots: true,
-        loop: true,
-        responsive: { 0: {items: 1}, 576: {items: 2}, 768: {items: 3}, 992: {items: 4} }
+        items: 2,
+    margin: 45,
+    autoplay: true,
+    loop: true,
+    nav: true,
+    dots: true,
+    responsive:{
+    0:   { items: 1 },   // phones
+    768: { items: 2 },   // tablets
+    1200:{ items: 3 }    // large screens
+  }
     });
+    
     
 })(jQuery);
 
